1c8aa258a191f112116db71d2d4ca31c937573bd
DevExpressExampleBot <fakeEmail>
2021-09-02 17:10:44 +0300

    Source auto update [skip ci]

--- VB/CartesianMap/MainWindow.xaml.vb
+++ VB/CartesianMap/MainWindow.xaml.vb
@@ -2,15 +2,15 @@
 Imports DevExpress.Xpf.Map
 
 Namespace CartesianMap
-    Partial Public Class MainWindow
-        Inherits Window
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+		End Sub
 
-        Private Sub VectorLayer_DataLoaded(ByVal sender As Object, ByVal e As DataLoadedEventArgs)
-            mapControl.ZoomToFitLayerItems()
-        End Sub
-    End Class
+		Private Sub VectorLayer_DataLoaded(ByVal sender As Object, ByVal e As DataLoadedEventArgs)
+			mapControl.ZoomToFitLayerItems()
+		End Sub
+	End Class
 End Namespace
